e8999ae669c6d58faacc7e85ca0d60c082ef2e1e
Alberto-vgdd <alberto.vgdd@gmail.com>
2017-04-27 19:43:48 +0200

    Added Camera FOV collider

---
+++ Assets/CameraEnemyTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnemyTracker : MonoBehaviour 
+{
+
+	// Use this for initialization
+    void Awake()
+    {
+        GlobalData.LockableEnemies = new LinkedList<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Enemy"))
+        {
+            GlobalData.LockableEnemies.AddLast(collider.transform);
+        }
+
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Enemy") && GlobalData.LockableEnemies.Contains(collider.transform))
+        {
+            GlobalData.LockableEnemies.Remove(collider.transform);
+        }
+
+    }
+}
